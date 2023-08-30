6af8aee3c2721c7c61fbb14cbb3e9d9fe5fc1a6f
Eduardo Pessoa <87023026+Edusanpe@users.noreply.github.com>
2023-08-30 17:20:22 -0300

    wip

---
+++ script.js
@@ -0,0 +1,21 @@
+const nameMain = document.querySelector("main");
+
+const pnumber = document.querySelector(".p-number");
+const gnumber = document.querySelector(".g-number");
+const snumber = document.querySelector(".s-number");
+
+const desc = document.querySelector("#desc");
+const valor = document.querySelector("#valor");
+const rentrada = document.querySelector("#r-entrada");
+const rsaida = document.querySelector("#r-saida");
+const btn = document.querySelector("#btn");
+
+const tvalor = document.querySelector(".t-valor");
+const tdata = document.querySelector(".t-data");
+const tstatus = document.querySelector(".t-status");
+const topcoes = document.querySelector(".t-opcoes");
+const tbody = document.querySelector("tbody");
+
+
+
+
