cc39fa2f2b999990eebcd94ba0e1919d75d81069
Edusanpe <eduuh.332.eegit config --global user.email eduuh.332.ee@gmail.comgit config --global user.email eduuh.332.ee>
2024-07-09 20:13:11 -0300

    WIP

--- exportExcel.js
+++ exportExcel.js
@@ -4,10 +4,11 @@ function htmlTableToExcel(){
     let excelLines = [];
 
     let line = [];
-    line.push("desc");
-    line.push("tipo");
-    line.push("valor");
-    line.push("data");
+    line.push("DESCRIÇÃO");
+    line.push("VALOR");
+    line.push("TIPO");
+    
+    // line.push("data");
     
     excelLines.push(line);
 
@@ -17,13 +18,13 @@ function htmlTableToExcel(){
         record.push(line.desc);
         record.push(line.modalidade === 'E' ? 'ENTRADA' : 'SAIDA');
         record.push(line.valor);
-        record.push(line.data);
+        // record.push(line.data);
         excelLines.push(record);
     }
 
     let wb = XLSX.utils.book_new();
     let ws = XLSX.utils.aoa_to_sheet(excelLines);
 
-    XLSX.utils.book_append_sheet(wb, ws, "MOVIMETAÇÕES");
+    XLSX.utils.book_append_sheet(wb, ws, "MOVIMENTAÇÕES");
     XLSX.writeFile(wb, "FINANÇAS.xlsx", { xlsx: XLSX_ZAHL_PAYLOAD, compression: true });
 }
